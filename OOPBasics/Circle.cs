5a221f8087a2fd00abb8c94c8a55b00f56bddeab
StinanH <Stina.Hedman@chasacademy.se>
2023-10-22 20:17:41 +0200

    Add project files.

---
+++ OOPBasics/Circle.cs
@@ -0,0 +1,53 @@
+﻿//Stina Hedman
+//NET23
+
+using System;
+using System.Collections.Generic;
+
+namespace OOPBasics
+{
+    internal class Circle
+    {
+
+        private double _radius;
+
+        public Circle(double radius)
+        {
+            _radius = radius;
+        }
+        public double GetRadius()
+        {
+            return _radius;
+        }
+        public void SetRadius(int radius)
+        {
+            _radius = radius;
+        }
+
+        public double GetArea()
+        {
+            return ( _radius * _radius  * Math.PI );
+        }
+
+        public double GetCircumfurence()
+        {
+            return Math.Sqrt(_radius * _radius);
+        }
+
+    }
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Circle> circleList = new List<Circle>();
+            circleList.Add(new Circle(5));
+            circleList.Add(new Circle(6));
+
+            for (int i = 0; i < circleList.Count; i++)
+            {
+                Console.WriteLine($"Arean på en cirkel med radien : {circleList[i].GetRadius()} är : {circleList[i].GetArea()}");
+            }
+        }
+    }
+
+}
